c792ce6227d23342eb5e32221f65a4412e6cdb34
Adrian Danao <adrian_danao01@yahoo.com>
2017-12-14 22:13:50 +0800

    Add files via upload

---
+++ 617A/Elephant.java
@@ -0,0 +1,11 @@
+//http://codeforces.com/problemset/problem/617/A
+import java.util.*;
+
+public class Elephant{
+	public static void main (String args[]){
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		input = (input+4)/5;
+		System.out.println(input);
+	}
+}
